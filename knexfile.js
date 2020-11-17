9c5fec95281ce25e374bf998f4a95a156c7964ef
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-11-16 23:44:50 -0500

    final

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.HEROKU_POSTGRESQL_GOLD_URL,
     migrations: {
       directory: "./database/migrations",
     },
